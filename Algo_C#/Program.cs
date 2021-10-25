9a817a0dae7823a37f07f1e9f10ad12453f108da
Trevor Engen <48308407+trevorengen@users.noreply.github.com>
2021-10-25 11:59:27 -0700

    C# version of algo.j

---
+++ Algo_C#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace algo_C_
+{
+    public class MinStack 
+    {
+        public List<int> stack;
+        public List<int> minStack;
+        public MinStack() 
+        {
+            stack = new List<int>();
+            minStack = new List<int>();
+        }
+        
+        public void Push(int val) 
+        {
+            stack.Add(val);
+            if (minStack.Count == 0 || stack[minStack[minStack.Count - 1]] >= val)
+            {
+                minStack.Add(stack.Count - 1);
+            }
+        }
+        
+        public void Pop() 
+        {
+            if (minStack[minStack.Count - 1] == stack.Count - 1)
+            {
+                minStack.RemoveAt(minStack.Count - 1);
+            }
+            stack.RemoveAt(stack.Count - 1);
+        }
+        
+        public int Top() 
+        {
+            return stack[stack.Count - 1];
+        }
+        
+        public int GetMin() 
+        {
+            return stack[minStack.Count - 1];
+        }
+    }
+}
